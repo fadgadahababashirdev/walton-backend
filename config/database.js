ceae1d2efa7c4d4b4ff20296f6b75a7f75f73824
fdahaba <138160871+fadgadahababashirdev@users.noreply.github.com>
2025-12-12 14:10:03 +0200

    using es7

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ export const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 5432,
-        dialect: 'postgres',
+        dialect: process.env.DB_DIALECT || 'postgres',
         logging: process.env.NODE_ENV === 'development' ? console.log : false,
         pool: {
             max: 5,
